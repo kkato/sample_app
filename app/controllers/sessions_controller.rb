32dbb30c56fe0242ab48c2dc9bf00c3237cb4ddb
kkato <kkato149@gmail.com>
2024-12-30 14:28:42 +0900

    Fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user # && user.authenticate(params[:session][:password])
-      reset_session
+    if user && user.authenticate(params[:session][:password])
+      reset_session      # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user
     else
